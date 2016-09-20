262e14da2d8a774dcfc50b156b0fb8b5e8ede440
Evans Murithi <murithievans80@gmail.com>
2016-09-20 20:20:53 +0300

    Initial commit

---
+++ elimika_backend/common/models/base.py
@@ -0,0 +1,66 @@
+import uuid
+
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.utils import timezone
+
+
+class BaseModel(models.Model):
+    """
+    Contains fields present in all models.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='+')
+    updated_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name='+')
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    deleted = models.BooleanField(default=True)
+
+    class Meta():
+        """Meta class for ``BaseModel``."""
+        ordering = ('-updated', '-created',)
+        abstract = True
+
+    def validate_updated_greater_than_created_date(self):
+        """
+        Validate that the ``updated`` field is greater than the ``created``
+        field.
+        """
+        if self.updated < self.created:
+            raise ValidationError({
+                'updated': (
+                    'The updated date cannot be in the past of created '
+                    'date.'
+                )
+            })
+
+    def preserve_created_by(self):
+        """
+        Ensures that ``created_by`` field is not overriden.
+        """
+        try:
+            original = self.__class__.objects.get(pk=self.pk)
+            self.created_by = original.created_by
+        except self.__class__.DoesNotExist:
+            pass
+
+    def clean(self, *args, **kwargs):
+        """Override clean method."""
+        self.validate_updated_greater_than_created_date()
+        super().clean(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        """Override save method."""
+        self.preserve_created_by()
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        """Override delete method."""
+        self.deleted = True
+        self.save(*args, **kwargs)
---
+++ elimika_backend/questions/models.py
@@ -0,0 +1,89 @@
+from django.core.exceptions import ValidationError
+from django.db import models
+
+from .common.models.base import BaseModel
+
+
+CATEGORY_CHOICES = (
+    ('some_type', 'Some type'),
+)
+
+
+class Question(BaseModel):
+    """
+    Hold fields for questions to be asked.
+    """
+
+    question = models.TextField()
+    tutor = models.ForeignKey(
+        'Tutor', on_delete=models.PROTECT, related_name='tutor_questions')
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=100)
+
+    def __str__(self):
+        """String representation."""
+        return self.question
+
+
+class Choice(BaseModel):
+    """
+    Hold choices for questions already present.
+    """
+
+    question = models.ForeignKey(
+        Question, on_delete=models.PROTECT, related_name='question_choices')
+    choice = models.CharField(max_length=255)
+    is_right = models.BooleanField(default=False)
+    tutor = models.ForeignKey(
+        'Tutor', on_delete=models.PROTECT, related_name='+')
+
+    def __str__(self):
+        """String representation."""
+        return self.choice
+
+    def validate_choice_tutor(self):
+        """
+        The ``tutor`` that created the ``question`` should be the one to create
+        its ``choice``.
+        """
+        if self.tutor is not self.question.tutor:
+            raise ValidationError({
+                'tutor': (
+                    'The tutor that created the referenced question should '
+                    'be the one to create its choice.'
+                )
+            })
+
+    def clean(self, *args, **kwargs):
+        """Override clean method."""
+        self.validate_choice_tutor()
+        super().clean(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        """Override save method."""
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+
+class Answer(BaseModel):
+    """
+    Holds answers for a particular question given by a learner.
+    """
+
+    question = models.ForeignKey(
+        Question, on_delete=models.PROTECT, related_name='question_answers')
+    choice = models.ForeignKey(
+        Choice, on_delete=models.PROTECT, related_name='+')
+    learner = models.ForeignKey(
+        'Learner', on_delete=models.PROTECT, related_name='learner_answers')
+
+    def __str__(self):
+        """String representation."""
+        return str(self.choice)
+
+    @property
+    def is_right_answer(self):
+        """
+        Evaluate if answer provided is the right one.
+        """
+        return (self.choice in
+                self.question.question_choices.filter(is_right=True))
